a9354aa1fd4f7023a8c3288775dbdf4baec019af
sabapathy <sabapathy80@gmail.com>
2024-03-20 16:04:39 +0530

    Update hello.cpp
    added class to test draw.io functionality

--- hello.cpp
+++ hello.cpp
@@ -2,9 +2,35 @@
 #include <conio.h>
 using namespace std;
 
+class Base1{
+int m_nVal;
+public:
+Base1();
+~Base1();
+void display();
+};
+
+void Base1::display()
+{
+  cout<< "Value of m_nVal is: " << m_nVal <<endl;
+}
+
+class Derived: public Base1{
+int m_nVal1;
+public:
+Derived(int n):m_nVal1(n), m_nVal(n*2){};
+~Derived();
+void display()
+{
+  super::display();
+  cout<< "Value of m_nVal1 is: " << m_nVal1 <<endl;
+}
+};
 int main()
 {
 cout<<"Hello world";
+Derived obj;
+obj.display();
 _getch();
 return 0;
 }
